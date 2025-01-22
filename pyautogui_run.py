5575f607d374bb226e916e700ecfd8b60b183e99
daehwan501.kim <daehwan501.kim@samsung.com>
2025-01-22 14:38:09 +0900

    update pyautogui code

--- pyautogui_run.py
+++ pyautogui_run.py
@@ -1,13 +1,56 @@
 import pyautogui as pg
 import time
 
+dx = 47
+dy = 62
+
+count = 0
 while True:
     try:
-        pattern = pg.locateOnScreen('edu/next_page_tooltip.png')
-        center = pg.center(pattern)
-        pg.moveTo(center.x+47, center.y+62)
+        # pattern = pg.locateOnScreen('edu/next_page_tooltip.png', confidence=0.95)
+        # center = pg.center(pattern)
+        center = pg.locateCenterOnScreen('edu/next_page_tooltip2.png', confidence=0.95, region = (1120, 660, 1260, 790))
+        pg.moveTo(center.x+dx, center.y+dy)
         pg.click()
-        print('next pattern found and clicked')
+        print('!')
     except:
-        print('pattern not found')
+        count += 1
+        if count<6:
+            print('.',end='')
+        else:
+            print('.')
+            count = 0
+    time.sleep(10)
+
+
+count = 0
+while True:
+    try:
+        # pattern = pg.locateOnScreen('edu/next_page_tooltip.png', confidence=0.95)
+        # center = pg.center(pattern)
+        center = pg.locateCenterOnScreen('edu/next_page_tooltip.png', confidence=0.95)
+        pg.moveTo(center.x+dx, center.y+dy)
+        pg.click()
+        print('1')
+    except:
+        try:
+            pattern = pg.locateOnScreen('edu/next_page_tooltip2.png', confidence=0.95)
+            center = pg.center(pattern)
+            pg.moveTo(center.x+47, center.y+62)
+            pg.click()
+            print('2')
+        except:
+            try:
+                pattern = pg.locateOnScreen('edu/next_page_tooltip3.png', confidence=0.95)
+                center = pg.center(pattern)
+                pg.moveTo(center.x+47, center.y+62)
+                pg.click()
+                print('3')
+            except:        
+                count += 1
+                if count<6:
+                    print('.',end='')
+                else:
+                    print('.')
+                    count = 0
     time.sleep(10)
